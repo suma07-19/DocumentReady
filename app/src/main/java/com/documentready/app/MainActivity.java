92f17170a22c020d421ae17f478df093e87c53d3
suma07-19 <sumasreek2010@gmail.com>
2026-09-06 21:36:29 +0530

    Implement MainActivity with WebView functionality

---
+++ app/src/main/java/com/documentready/app/MainActivity.java
@@ -0,0 +1,37 @@
+package com.documentready.app;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+
+public class MainActivity extends Activity {
+
+    private WebView webView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        webView = new WebView(this);
+        webView.setWebViewClient(new WebViewClient());
+
+        WebSettings settings = webView.getSettings();
+        settings.setJavaScriptEnabled(true);
+        settings.setDomStorageEnabled(true);
+
+        webView.loadUrl("https://classy-halva-c0725d.netlify.app/");
+
+        setContentView(webView);
+    }
+
+    @Override
+    public void onBackPressed() {
+        if (webView.canGoBack()) {
+            webView.goBack();
+        } else {
+            super.onBackPressed();
+        }
+    }
+}
